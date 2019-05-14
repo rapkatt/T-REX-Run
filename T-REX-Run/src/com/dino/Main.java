30622be46891e165b43838f365a1aeb4878433ba
rapkatt <rapkat99@gmail.com>
2019-05-14 19:01:34 +0600

    change Title

--- T-REX-Run/src/com/dino/Main.java
+++ T-REX-Run/src/com/dino/Main.java
@@ -10,7 +10,7 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application{
-    public static final String TITLE = "Dino Run";
+    public static final String TITLE = "T-REX RUN";
 
     public static void main(String[] args) {
         launch(args);
